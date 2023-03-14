5c94c85d844873c464c3191392217150438974d8
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-03-14 10:27:55 +0100

    Adds basic server config and Mongo connection

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import Express from "express"
+import listEndpoints from "express-list-endpoints"
+import cors from "cors"
+import mongoose from "mongoose"
+
+const server = Express()
+const port = process.env.PORT || 3001
+
+// **************************************** MIDDLEWARES *****************************************
+server.use(cors())
+server.use(Express.json())
+
+// ****************************************** ENDPOINTS *****************************************
+
+// **************************************** ERROR HANDLERS **************************************
+
+mongoose.connect(process.env.MONGO_URL)
+
+mongoose.connection.on("connected", () => {
+  console.log("✅ Successfully connected to Mongo!")
+  server.listen(port, () => {
+    console.table(listEndpoints(server))
+    console.log(`✅ Server is running on port ${port}`)
+  })
+})
